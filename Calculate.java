8311441a8d82709e8db78e390f8947e101085e9c
Ivan Dobrokhotov <ozeki@yandex.ru>
2017-05-03 17:28:08 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,11 @@
 public class Calculate {
     public static void main(String[] arg){
         System.out.println("Calculate...");
+        int first;
+        first = Integer.valueOf(arg[0]);
+        int second;
+        second = Integer.valueOf(arg[1]);
+        int summ = first + second;
+        System.out.println("Sum = " + summ);
     }
 }
